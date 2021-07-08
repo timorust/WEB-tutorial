b7e6caf05843db8c512f4181bd9b0cc512d672af
timor.rust <75886852+timorust@users.noreply.github.com>
2021-07-08 16:28:18 +0300

    async

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const os = require('os');
 console.log(`free memory is =>${os.freemem()}`);
 console.log(`total memory is =>${os.totalmem}`);
 
-const fs = require('fs')
-const files = fs.readdirSync('./')
-console.log(files);
+// const fs = require('fs')
+// const files = fs.readdirSync('./')
+// console.log(files);
+
+
+const fs = require('fs');
+
+fs.readdir(__dirname, (err, file) => {
+  
+  if(err) {
+    console.log(`error ${err}`)
+  }
+  else {
+    console.log(`files is =>${file}`)
+  }
+})
