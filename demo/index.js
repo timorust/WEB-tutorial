040e651ec8fff791808adc47b3d46a680d24b260
timor.rust <75886852+timorust@users.noreply.github.com>
2021-07-09 01:29:46 +0300

    error file

--- demo/index.js
+++ demo/index.js
@@ -4,28 +4,62 @@ const path = require('path')
 
 const server = http.createServer((req, res) => {
 
-  if(req.url === '/') {
-    fs.readFile(path.join(__dirname, 'test', 'index.html'), (err, data) => {
-      if(err) {
-        throw err
-      }
-      res.writeHead(200, {
-       'Content-type': 'text/html'
-      })
-       res.end(data)
-    })  
-  } 
-  else if(req.url === '/contact') {
-    fs.readFile(path.join(__dirname, 'test', 'contact.html'), (err, data) => {
-      if(err) {
-        throw err
-      }
-      res.writeHead(200, {
-       'Content-type': 'text/html'
-      })
-       res.end(data)
-    })  
+  // if(req.url === '/') {
+  //   fs.readFile(path.join(__dirname, 'test', 'index.html'), (err, data) => {
+  //     if(err) {
+  //       throw err
+  //     }
+  //     res.writeHead(200, {
+  //      'Content-type': 'text/html'
+  //     })
+  //      res.end(data)
+  //   })  
+  // } 
+  // else if(req.url === '/contact') {
+  //   fs.readFile(path.join(__dirname, 'test', 'contact.html'), (err, data) => {
+  //     if(err) {
+  //       throw err
+  //     }
+  //     res.writeHead(200, {
+  //      'Content-type': 'text/html'
+  //     })
+  //      res.end(data)
+  //   })  
+  // }
+
+  let filePath = path.join(__dirname, 'test', req.url === '/' ? 'index.html' : req.url)
+
+  const ext = path.extname(filePath)
+
+  if(!ext) {
+    filePath += '.html'
   }
+
+
+  console.log(filePath)
+
+  fs.readFile(filePath, (err, context) => {
+    if(err) {
+      fs.readFile(path.join(__dirname, 'test', 'error.html'), (err, data) => {
+        if(err) {
+          res.writeHead(500)
+          res.end('Error')
+        }
+        else {
+          res.writeHead(200, {
+            'Content-Type': 'text/html'
+          })
+          res.end(data)
+        }
+      })
+    }
+    else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      })
+      res.end(context)
+    }
+  })
 })
 
 server.listen(4000, () => {
