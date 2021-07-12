df1497b197e1533dd06f7ddfc3b3eb2ad45a34cd
timor.rust <75886852+timorust@users.noreply.github.com>
2021-07-13 01:13:55 +0300

    ERROR

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-const fs = require('fs');
-const path = require('path');
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-// const files = fs.readdirSync('./');
-// console.log('[sync] files is =>' + files)
-// console.log(files.find(item => item.toUpperCase() == 'app.js'.toUpperCase()))
+const logger = require('./logger');
+console.log(`import require:`)
+console.log(logger)
 
-readline.question('Which file you want to search (full-name)? ', file_name => {
-  console.log(`Searching is =>${file_name}!`);
-  readline.close();
+console.log(`end point require is =>${logger.end_point}`)
 
-  fs.readdir(__dirname, (err, files) => {
-    if (err) {
-      console.log(`Erorr ${err}`)
-    }
-    else {
-      console.log(`[sync] file is =>${files}`)
-    }
-    if (files.find(item => item.toUpperCase() == file_name.toUpperCase())) {
-      console.log(`file name is =>${file_name} exist in =>${__dirname} folder`)
-    }
-    else {
-      console.log(`file name is =>${file_name} NOT exist in =>${__dirname} folder`)
-    }
-  })
-})
+logger.log('===== system start up')
+
+const fs = require('fs')
+const files = fs.readdirSync('./') // sync -- bad!!!!
+console.log('[sync] ' + files)
+
+fs.readdir('$', (err, files) => {
+    if (err) 
+       logger.log('ERROR in fs.readdir of folder $: ' + err)
+    else 
+       console.log(`[async] Files ${files}`)
+ })
+
+logger.log('====== system shut down')
---
+++ logger.js
@@ -0,0 +1,10 @@
+ const url = 'https://github.com/timorust';
+
+ function log(message) {
+   console.log(`Logging: ${message}`)
+ }
+
+//  console.log(module);
+
+ module.exports.log = log;
+ module.exports.end_point = url;
