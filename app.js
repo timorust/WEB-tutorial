cc4539fe6b742e1f6402ea7bf90c417f3e27a31e
timor.rust <75886852+timorust@users.noreply.github.com>
2021-07-13 14:40:34 +0300

    parseInt

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 
+const logger = require('./logger')
 const EventEmitter = require('events');
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 const largenum_emitter = new EventEmitter;
 const uppercase_emitter = new EventEmitter;
-const logger = require('./logger')
+
+let x, y = 0;
+let result = 0;
 
 uppercase_emitter.on('trying_to_divide_by_zero', (_) => {
   // event-handler => what to do event was triggered?
@@ -16,15 +25,6 @@ uppercase_emitter.on('trying_to_divide_by_zero', (_) => {
   logger.log(`ERROR LARGE NUMBER ${_} OCCURED!`);
 })
 
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-   
-
-  let x,y = 0;
-  let result = 0;
   readline.question('Enter 1st number ?', _x => {
       x = _x;
 
@@ -34,7 +34,7 @@ const readline = require('readline').createInterface({
           uppercase_emitter.emit('trying_to_divide_by_zero', y)
         }
         
-        else if((result = x * y) > 100){
+        else if((result = parseInt(x) * parseInt(y)) > 100) {
           largenum_emitter.emit('result_sum_above_100', result)
         }
         else {
